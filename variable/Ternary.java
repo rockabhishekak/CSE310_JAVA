d04e1d2521f778814564c4a4667234e8d8f1db1f
rockabhishekak <abhishek.kumar20248@lpu.in>
2026-02-02 15:20:31 +0530

    Tarnery

---
+++ variable/Ternary.java
@@ -0,0 +1,13 @@
+// using ternary find max in 3 numbers;
+class Ternary{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 11;
+        int c = 12;
+        int result = (a>b)?(a>c ? a:c):(b>c?b:c);
+        System.out.println(result);
+
+
+
+    }
+};
